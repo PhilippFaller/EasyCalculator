01e668f19e87bf3ee08b837668658b9acf3263b8
Philipp <philipp.faller@yahoo.de>
2014-07-01 13:07:19 +0200

    Completely new Code. Reads multiple operators before calculating.

--- main.c
+++ main.c
@@ -1,117 +1,50 @@
-#include "at89s8253.h"
+#include <at898252.h>
 
-#define IN P0
-#define OUT_H P1
-#define OUT_L P2
+sfr IN = 0x80;
 
-#define FAIL 0xFFFF
-
-#define DIVIDE 0x10
-#define MULTIPLY 0x11
-#define SUBTRACT 0x12
-#define ADD 0x13
+#define EMPTY 0xFF
 #define CLEAR 0x14
 #define EQUALS 0x15
-#define NO_OPERATION 0
-#define EMPTY 0xFF
+#define BUFFER_SIZE 127
 
 void read();
 int calculate();
-void show(int parameter);
-
+void show(int number);
 unsigned char getFromIn();
 
-void reset();
-
-unsigned char leftOperand = 0;
-unsigned char operator = 0;
-unsigned char rightOperand = 0;
+//const int(*)(int a, int b) operations[4];
+int buffer [BUFFER_SIZE];
+int iterator = 0; 
 
 main(){
-    while(1){
-        read();
-        show(calculate());
-    }
+	while(1){
+		read();
+		show(calculate());
+	}
 }
 
 void read(){
-    int i;
-    for(i = 0; i < 4; i++){
-        unsigned char temp = getFromIn();
-        if(temp <= 0xF){
-            leftOperand <<= 4;
-            leftOperand |= temp;
-            show(leftOperand);
-        }
-
-        else{
-            switch(temp){
-            case CLEAR: reset();
-                	return;
-            case EQUALS: operator = NO_OPERATION;
-                	return;
-            default: 	operator = temp;
-            		show(operator);
-                	goto secondInput;
-            }
-        }
-    }
-
-    unsigned char temp = getFromIn();
-    show(temp);
-    if(temp > 0xF && temp < CLEAR) operator = temp;
-    else {
-	leftOperand = FAIL;
-	operator = NO_OPERATION;
-	return;
-    }
-
-secondInput:for(i = 0; i < 4; i++){
-        unsigned char temp = getFromIn();
-        if(temp <= 0xF){
-            rightOperand <<= 4;
-            rightOperand |= temp;
-            show(rightOperand);
-        }
-        else{
-            switch(temp){
-            case CLEAR: reset();
-                return;
-            case EQUALS: return;
-            default: leftOperand = FAIL; 
-                     operator = NO_OPERATION;
-                     return;
-            }
-        }
-    }
+	unsigned char input = 0;
+	int i;
+	for(i = 0; i < 4; i++){
+	    int temp = getFromIn();
+		if(temp < 0xF) input = (input << 4) | temp;
+		else if(temp < CLEAR){
+			buffer[iterator++] = input;
+			//if(iterator > 0 && buffer[iterator - 1] < CLEAR)
+			// TODO evtl prüfen: Zwei Operatoren hintereinander??	
+			buffer[iterator] = temp;
+			break;
+		} 
+		else  return;
+	}
+	read();
 }
 
-int calculate(){
-    switch(operator){
-    case DIVIDE: return leftOperand / rightOperand;
-    case MULTIPLY: return leftOperand * rightOperand;
-    case SUBTRACT: return leftOperand - rightOperand;
-    case ADD: return leftOperand + rightOperand;
-    case NO_OPERATION: return leftOperand;
-    default: //Fehler
-        return FAIL;
-    }
-}
-
-void show(int parameter){
-    OUT_L = parameter;
-    OUT_H = parameter >> 8;
-}
-
-unsigned char getFromIn(){
-    while(IN == EMPTY);
-    unsigned char temp = IN;
-    while(IN == temp);
-    return temp;
-}
-
-void reset(){
-    leftOperand = 0;
-    operator = 0;
-    rightOperand = 0;
+unsigned char getFromIn() {
+	unsigned char temp;
+ 	while(IN == EMPTY);
+	temp = IN;
+	while(IN == temp);
+	return temp;
 }
